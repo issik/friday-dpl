c184d2af668b14e94da61c255258a08d61917510
isaac bustos <isaac.youthboard@futureindesign.org>
2017-11-17 10:14:39 -0700

    ihope to have fnctional code next friday

---
+++ friday.rb
@@ -0,0 +1,30 @@
+# gets user input user inputs string
+word = true
+def palendromecheckerls
+  
+  "test you palendorme"
+   gets = word.length
+if word != true
+ end
+ end
+# string is assigned value of true
+# string is reversed and if it amtches the same value as the stored
+class String
+  def reverse_inplace!
+      half_length = self.length / 2
+      half_length.times {|i| self[i], self[-i-1] = self[-i-1], self[i] }
+      self
+  end
+end
+
+#array it returns true
+#if both are true
+# (user input) if [] is equal both backwards
+#puts yes!
+
+puts  "is #{word.length}a palendrome?"
+
+
+
+
+palendromechecker
